1d4715ae5b8f17fb85e2aa5b114179e0d8540e55
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-05 20:09:47 +0530

    fixed error in js character is undefined

--- index.js
+++ index.js
@@ -5,22 +5,28 @@ const emojis = [];
 async function getEmojiData() {
   const url = `https://emoji-api.com/emojis?access_key=4a35ab0d75bfe3ada1b73922320f1a59e90fb89d`;
 
-  const response = await fetch(url).then((res) => res.json());
-  //   console.log(response);
+  try {
+    const response = await fetch(url).then((res) => res.json());
+    //   console.log(response);
 
-  for (let i = 0; i < 1000; i++) {
-    emojis.push({
-      character: response[i].character,
-      title: response[i].unicodeName.slice(5),
+    for (let i = 0; i < 1000; i++) {
+      emojis.push({
+        character: response[i].character,
+        title: response[i].unicodeName.slice(5),
+      });
+    }
+    //   console.log(emojis);
+    emojiContainer.addEventListener("click", () => {
+      const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+      emojiContainer.innerText = emoji.character;
+      //   console.log(emoji.title);
+      EmojiTitle.innerText = emoji.title;
     });
+  } catch (error) {
+    console.log("Something went wrong..." + error);
+    emojiContainer.innerText = "❌";
+    EmojiTitle.innerText = "Something went wrong... Try again later";
   }
-  //   console.log(emojis);
 }
 
 getEmojiData();
-emojiContainer.addEventListener("click", () => {
-  const emoji = emojis[Math.floor(Math.random() * 1001)];
-  emojiContainer.innerText = emoji.character;
-  //   console.log(emoji.title);
-  EmojiTitle.innerText = emoji.title;
-});
